a94cc661d7194369aea3e092cb51b310a456b5f6
Blastiq <blastiquemajar@gmail.com>
2022-08-08 18:43:31 +0100

    version 1 commit

---
+++ app.js
@@ -0,0 +1,55 @@
+// Create a node app
+
+const express = require ("express");
+const https = require ("https");
+const bodyParser = require("body-parser");
+//Initialize a new express app
+const app = express();
+// Listening port for the app
+const port = 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/index.html")
+
+});
+
+app.post("/", function(req, res){
+  const query = req.body.cityName;
+  const apiKey = "a950156b58e2bf40c0a8ae4b2a75c66f"
+  const units = "metric"
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + units + "";
+
+
+
+
+  https.get(url, function(response){
+    console.log(response.statusCode);
+
+  // Here we got a hold of data and passed it through JSON to a javascript object
+    response.on("data", function(data){
+      const weatherData = JSON.parse(data)
+      const temp = weatherData.main.temp
+      const desc = weatherData.weather[0].description
+      const icon = weatherData.weather[0].icon
+      const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+      res.write("<h1>The Temperature in " + query + " is " + temp + " Degrees Celcius. </h1>")
+      res.write("<p>The weather is currently "  + desc + "<p>");
+      res.write("<img src=" + imageURL + ">");
+      res.send()
+
+    })
+
+  })
+})
+
+/*
+
+
+*/
+
+
+app.listen(port, function(){
+  console.log("Serve is running on port " + port);
+});
